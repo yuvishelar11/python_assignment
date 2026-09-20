785aaf2dc7af13d42a9f673704059f1702a0b7fe
yuvishelar11 <shelaryuvi1118@gmail.com>
2026-09-20 18:29:45 +0530

    uploaded assignment

---
+++ Exercise2.py
@@ -0,0 +1,148 @@
+
+#Quetion no:1
+
+x = 15
+y = 4 
+
+print("addition ",x+y)
+print("substraction " , x-y)
+print("multiplication",x*y)
+print("division ",x/y)
+print("modulus",x%y)
+print("floor division ",x//y)
+print("exponentiation ",x**y)
+
+#Quetion no: 2
+
+num = 50
+print(num)
+
+num += 10
+print(num)
+num -= 5
+print(num)
+num *= 2
+print(num)
+num /= 4
+print(num)
+num %= 5
+print(num)
+num //= 3
+print(num)
+
+#Quetion no:3
+a = 25 
+b = 30
+
+print(a<b)
+print(a>b)
+print(a<=b)
+print(a>=b)
+print(a==b)
+print(a!=b)
+
+#Quetion no:4
+
+x=10
+y=20
+z=30
+
+print(x<y and y<z)
+print(x<y or y<z)
+print(not(x==y))
+print(not(y==z))
+
+#Quetion no:5
+
+a = 100
+b= 100
+
+list1=[1,2,3]
+list2=[1,2,3]
+
+print(id(a))
+print(id(b))
+
+print(id(list1))
+print(id(list2))
+
+print(id(a) is id(b))
+print(id(list1) is not id(list2))
+
+print(list1==list2)
+
+print(list1 is not list2)
+
+
+#Quetion no :6
+color= ["red","blue","green","yellow"]
+
+
+print("blue" in color)
+print("red" in color)
+print("yellow" in color)
+print("green" in color )
+print("black" in color)
+print("red" not in  color)
+
+#Quetion no: 7
+
+
+x = 12     #1100
+y = 10     #1010
+
+print(x&y)
+print(x|y)
+print(x^y)
+print(~x)
+print(x<<2)
+print(x>>2)
+
+
+#Quetion no:8
+
+intVal = 10
+flotVal = 5.5
+stringVal = "python"
+
+print(type(intVal))
+print(type(flotVal))
+print(type(stringVal))
+
+
+ans= intVal+flotVal
+print(ans)
+print(type(ans))
+
+ans1= intVal*flotVal
+print(ans1)
+print(type(ans1))
+
+ans2= intVal/flotVal
+print(ans2)
+print(type(ans2))
+
+#Quetion no:9
+
+first="Hello" 
+second="world"
+
+print(first+second)
+print(first*2)
+print(second*2)
+
+
+#Quetion no:10
+
+a = 10
+b = 5
+c = 3
+print(a)
+print(b)
+print(c)
+print(a+b*c)
+print((a+b)*c)
+print(a**b//c)
+print(a<b and b>c or a==10)
+
+
