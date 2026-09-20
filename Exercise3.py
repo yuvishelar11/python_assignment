9f9339c66d0281ec0db0799140e4490df18ae336
yuvishelar11 <shelaryuvi1118@gmail.com>
2026-09-20 18:30:47 +0530

    Add files via upload

---
+++ Exercise3.py
@@ -0,0 +1,52 @@
+
+
+#Quetion no:1
+a=int(input("Enter the integer:"))
+print("you entered no. is ",a)
+
+#Quetion no:2
+b=input("String :")
+print("you entered string is ",b)
+
+#Quetion no:3
+c =float(input("enter the float no."))
+print("entered float no. is ",c)
+
+#Quetion no:4
+x = int(input("enter the no. "))
+y = int(input("enter the no. "))
+print("sum",x+y)
+
+#Quetion no:5
+name=input("enter your name :")
+age=int(input("enter your age:"))
+print("Name:",name)
+print("Age:",age)
+
+#Quetion no:6
+x=float(input("enter the no."))
+y=float(input("enter the no."))
+z=float(input("enter the no."))
+
+average= (x+y+z)/3
+print(average)
+
+#Quetion no:7
+x = int(input("enter the number"))
+print(type(x))
+
+
+#Quetion no:8
+name = input("enter the the name :")
+print(type(name))
+
+#Quetion no:9
+a = float(input("enter the number:"))
+print(type(a))
+
+#Quetion no:10
+x = int(input("enter the no.:"))
+y = int(input("enter the no.:"))
+print("multiplication:",x*y)
+
+
