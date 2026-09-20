2eb11955324f57441efc6b0f912a36615bb030cb
yuvishelar11 <shelaryuvi1118@gmail.com>
2026-09-20 18:31:54 +0530

    uploaded assignment

---
+++ Exercise4.py
@@ -0,0 +1,90 @@
+#program1
+
+x = 10
+y = 20
+print(x+y)
+
+#program2
+
+x = int(input("enter the num:"))
+if x>0:
+    print(x)
+
+
+
+
+#program3
+
+x = int(input("enter no.:"))
+if x>0:
+    print(x)
+
+    if x>0:
+        print(x)
+
+
+
+#program4
+
+x = int(input("Enter the no. :"))
+if x>0:
+    print(x)
+
+else:
+    print(x)
+
+
+#program5
+
+x = int(input("enter the no.:"))
+if x>0:
+    print(x)
+
+else:
+    print("in else",x)
+
+
+#program6
+
+#program7
+
+x = int(input("enter the number:"))
+if x>0:
+    print(x)
+
+if x==0:
+    print(x)
+
+if x<0:
+    print(x)
+
+#program8
+
+x = int(input("enter the no.:"))
+if x>0:
+    print(x)
+
+elif x==0:
+    print(x)
+
+else:
+    print(x)
+#program9
+
+#program10
+
+num1= 10
+num2 = 20
+sumdata= num1+num2
+subdata= num2-num1
+if num1>num2:
+    print(sumdata)
+
+else:
+    print(subdata)
+
+
+
+
+
+
