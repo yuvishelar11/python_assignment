45cf82f001922df11f73ab0aaa1166c8bf7e5904
yuvishelar11 <shelaryuvi1118@gmail.com>
2026-09-20 18:21:51 +0530

    uploaded assignment

---
+++ Exercise1.py
@@ -0,0 +1,158 @@
+#Quetion 1:
+
+print("hello Core2web")
+
+#Quetion 2:
+
+a = 10
+print(a)
+
+#Quetion 3:
+
+b = 10.12
+print(b)
+
+#Quetion 4:
+
+c =12+6j
+print(c)
+
+#Quetion 5:
+d = "yuvraj"
+print(d)
+
+#Quetion 6:
+
+k= True
+print(k)
+
+#Quetion 7:
+
+a = 25
+print(type(a))
+
+#Quetion 8:
+
+b = 31.5
+print(type(b))
+
+#Quetion 9:
+
+s = 12
+t = 16
+u = 3.14
+
+print(s+t+u)
+
+#Quetion 10:
+
+list = [ "mathematics" , "marathi" , "hindi" , "english" , "Scinece"]
+
+print(list[0])
+print(list[1])
+print(list[2])
+print(list[3])
+
+
+#Quetion 11:
+
+tup = ( 10,20,30,40,50,60,70 )
+
+print(tup)
+
+#Quetion 12:
+
+list=[ 10,20,30,40]
+sum = list[0]+list[1]+list[2]+list[3]
+
+print(sum)
+
+#Quetion 13:
+
+set= { 1,2,3,4,5,1,3,2,8,8,7,4,1}
+
+print(set)
+
+
+#Quetion 14 :
+
+a=[ 12,23,'come','go',12]
+print(a)
+ 
+a[3]='core2web'
+print(a)
+
+#Quetion 15/16:
+
+dict={ 'name':"yuvraj", 'age':19 , 'marks': 71.33 }
+
+print(dict)
+print(dict['name'])
+print(dict['age'])
+print(dict['marks'])
+
+
+dict['name']="parth"
+
+print(dict)
+
+#Quetion 17:
+
+dict= { 0:10 ,1:20,2:25,3:30,4:40}
+print(dict)
+print(dict[0])
+print(dict[1])
+print(dict[2])
+print(dict[3])
+print(dict[4])
+
+result= dict[2]+dict[3]
+print(result)
+
+#Quetion 18:
+
+b = None 
+print(b)
+print(type(b))
+
+#Quetion 19:
+
+x = 'yuvraj shelar'
+y = " yuvraj shelar"
+z = ''' yuvraj shelar '''
+
+
+print(x)
+print(y)
+print(z)
+
+
+#Quetion 20:
+
+
+
+intval=11
+floatval=22.21
+compval= 1+3j
+strval= 'yuvi'
+list=[1,2,3,4,5]
+tup=(1,2,3,4,5)
+dict={ 1:21,2:13,3:89}
+set={1,2,3,4,9}
+bool=False
+yu=None
+
+
+
+print(type(intval))
+print(type(floatval))
+print(type(compval))
+print(type(strval))
+print(type(list))
+print(type(tup))
+print(type(dict))
+print(type(set))
+print(type(bool))
+print(type(yu))
+
+
